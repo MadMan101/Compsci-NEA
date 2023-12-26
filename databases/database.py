6579c5fa445df016fb3a8f5eb708a25337d963c4
MadMan101 <m.o.trafford@gmail.com>
2023-12-27 02:44:06 +0400

    categories functional + income sys

--- databases/database.py
+++ databases/database.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime
 from categories.category import Category
 
 class BudgetDatabase:
@@ -14,7 +15,8 @@ class BudgetDatabase:
             CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 user TEXT NOT NULL,
-                pass REAL NOT NULL
+                pass REAL NOT NULL,
+                funds REAL DEFAULT 0
             )
         ''')
 
@@ -37,19 +39,6 @@ class BudgetDatabase:
                 FOREIGN KEY (user_id) REFERENCES users(id)
             )
         ''')
-        
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS income (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                user_id INTEGER,
-                source TEXT NOT NULL,
-                amount REAL NOT NULL,
-                date DATE NOT NULL,
-                income_type_id INTEGER,
-                FOREIGN KEY (user_id) REFERENCES users(id),
-                FOREIGN KEY (income_type_id) REFERENCES income_types(id)
-            )
-        ''')
 
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS goals (
@@ -63,15 +52,20 @@ class BudgetDatabase:
         ''')
 
         cursor.execute('''
-            CREATE TABLE IF NOT EXISTS expenses (
+            CREATE TABLE IF NOT EXISTS transactions (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 user_id INTEGER,
                 category_id INTEGER,
+                goal_id INTEGER,
+                income_id INTEGER,
                 amount REAL NOT NULL,
                 description TEXT,
                 date_spent DATE NOT NULL,
                 FOREIGN KEY (user_id) REFERENCES users(id),
                 FOREIGN KEY (category_id) REFERENCES categories(id)
+                FOREIGN KEY (goal_id) REFERENCES goals(id),
+                FOREIGN KEY (income_id) REFERENCES income_types(id),
+                CHECK ((category_id IS NULL AND goal_id IS NOT NULL AND income_id IS NULL) OR (category_id IS NOT NULL AND goal_id IS NULL AND income_id IS NULL) OR (category_id IS NULL AND goal_id IS NULL AND income_id IS NOT NULL))
             )
         ''')
 
@@ -94,7 +88,7 @@ class BudgetDatabase:
         cursor = self.conn.cursor()
         cursor.execute("SELECT pass FROM users WHERE user = ?", (user,))
         pswd = cursor.fetchone()[0]
-        cursor.execute("SELECT id FROM users WHERE user = ?", (user))
+        cursor.execute("SELECT id FROM users WHERE user = ?", (user,))
         self.id = cursor.fetchone()[0]
         return self.id if pswd == password else -1
 
@@ -102,7 +96,7 @@ class BudgetDatabase:
         valid = False 
 
         cursor = self.conn.cursor()
-        cursor.execute("SELECT name FROM categories WHERE name = ?", (name,))
+        cursor.execute("SELECT name FROM categories WHERE name = ? AND user_id = ?", (name, self.id,))
         if len(cursor.fetchall()) == 0:
             cursor.execute("INSERT INTO categories (user_id, name, budget) VALUES (?, ?, ?)", (self.id, name, budget_limit))
             valid = True
@@ -110,9 +104,21 @@ class BudgetDatabase:
 
         return valid
     
+    def insert_source(self, name):
+        valid = False 
+
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT name FROM income_types WHERE name = ? AND user_id = ?", (name, self.id,))
+        if len(cursor.fetchall()) == 0:
+            cursor.execute("INSERT INTO income_types (user_id, name) VALUES (?, ?)", (self.id, name,))
+            valid = True
+        self.conn.commit()
+
+        return valid
+    
     def get_category(self, name):
         cursor = self.conn.cursor()
-        cursor.execute("SELECT id FROM categories WHERE name = ?", (name,))
+        cursor.execute("SELECT id FROM categories WHERE name = ? AND user_id = ?", (name, self.id,))
         id = cursor.fetchone()[0]
         cursor.execute("SELECT budget FROM categories WHERE id = ?", (id,))
         budget = cursor.fetchone()[0]
@@ -121,32 +127,92 @@ class BudgetDatabase:
 
         return [id, name, budget, spent]
     
+    def get_income_type_id(self, name):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT id FROM income_types WHERE name = ? AND user_id = ?", (name, self.id,))
+        return cursor.fetchone()[0]
+    
+    def add_expense(self, misc_id, amount, description="", goal=False):
+        cursor = self.conn.cursor()
+
+        if goal:
+
+            cursor.execute("INSERT INTO expenses (user_id, goal_id, amount, description, date_spent) VALUES (?, ?, ?, ?, ?)", (self.id, misc_id, amount, description, datetime.now().date()))
+            cursor.execute("UPDATE goals SET ")
+        else:
+
+            cursor.execute("INSERT INTO transactions (user_id, category_id, amount, description, date_spent) VALUES (?, ?, ?, ?, ?)", (self.id, misc_id, amount, description, datetime.now().date()))
+            cursor.execute("UPDATE categories SET spent = spent + ? WHERE id = ?", (amount, misc_id,))
+            cursor.execute("UPDATE users SET funds = funds - ? WHERE id = ?", (amount, self.id))
+        
+        self.conn.commit()
+
     def get_category_names(self):
         cursor = self.conn.cursor()
         cursor.execute("SELECT name FROM categories WHERE user_id = ?", (self.id,))
-        return cursor.fetchall()
+        results = cursor.fetchall()
+        return [result[0] for result in results]
 
     def get_income_names(self):
         cursor = self.conn.cursor()
         cursor.execute("SELECT name FROM income_types WHERE user_id = ?", (self.id,))
-        return cursor.fetchall()
-
-    def get_id(self, name):
-        cursor = self.conn.cursor()
-        cursor.execute("SELECT id FROM categories WHERE name = ?", (name))
-        return cursor.fetchall()
+        results = cursor.fetchall()
+        return [result[0] for result in results]
     
-    def get_budget(self, id):
+    def get_funds(self):
         cursor = self.conn.cursor()
-        cursor.execute("SELECT budget_limit FROM categories WHERE id = ?", (id))
-        return cursor.fetchall()
-
-    def insert_expense(self, category_id, amount):
+        cursor.execute("SELECT funds FROM users WHERE id = ?", (self.id,))
+        return cursor.fetchone()[0]
+    
+    def add_income(self, income_id, amount, description):
         cursor = self.conn.cursor()
-        cursor.execute("INSERT INTO expenses (category_id, amount) VALUES (?, ?)", (category_id, amount))
+        cursor.execute("INSERT INTO transactions (user_id, income_id, amount, description, date_spent) VALUES (?, ?, ?, ?, ?)", (self.id, income_id, amount, description, datetime.now().date()))
+        cursor.execute("UPDATE users SET funds = funds + ? WHERE id = ?", (amount, self.id))
         self.conn.commit()
 
     def get_expenses(self, category_id):
         cursor = self.conn.cursor()
-        cursor.execute("SELECT amount FROM expenses WHERE category_id = ?", (category_id,))
-        return cursor.fetchall()
+
+        cursor.execute('''
+            SELECT date_spent, amount, description
+            FROM transactions
+            WHERE category_id = ? AND user_id = ?
+        ''', (category_id, self.id,))
+
+        expenses = cursor.fetchall()
+        return expenses
+    
+    def get_transactions(self):
+        cursor = self.conn.cursor()
+
+        cursor.execute('''SELECT date_spent, amount, description, income_id FROM transactions  WHERE user_id = ?''', (self.id,))
+        transactions = cursor.fetchall()
+
+        cursor.execute("SELECT category_id, goal_id, income_id FROM transactions WHERE user_id = ?", (self.id,))
+        ids = cursor.fetchall()
+
+        types = []
+
+        for id in ids:
+            if id[0] is not None:
+                cursor.execute("SELECT name FROm categories WHERE id = ?", (id[0],))
+                types.append(cursor.fetchone())
+            elif id[1] is not None:
+                cursor.execute("SELECT name FROM goals WHERE id = ?", (id[1],))
+                types.append(cursor.fetchone())
+            else:
+                cursor.execute("SELECT name FROM income_types WHERE id = ?", (id[2],))
+                types.append(cursor.fetchone())
+
+        mapped_transactions = []
+        i = 0
+
+        for transaction in transactions:
+            income_id = transaction[3]
+            if income_id is None:
+                mapped_transactions.append([transaction[0], transaction[1] * -1, transaction[2], types[i]])
+            else:
+                mapped_transactions.append([transaction[0], transaction[1], transaction[2], types[i]])
+            i = i + 1
+
+        return mapped_transactions
--- ui/app.py
+++ ui/app.py
@@ -3,6 +3,7 @@ from tkinter import ttk, messagebox
 from categories.category import Category
 from expenses.expense import Expense
 from databases.database import BudgetDatabase
+import configparser
 
 class BudgetTrackerApp:
     def __init__(self, root):
@@ -11,39 +12,103 @@ class BudgetTrackerApp:
 
         self.db = BudgetDatabase()
 
+        # Load saved credentials
+        saved_credentials = self.load_saved_credentials()
+
         # Create login window
         self.login_window = tk.Toplevel(self.root)
         self.login_window.title("Login")
         self.login_window.geometry("400x250")
+        self.login_window.attributes("-topmost", True)
 
         # Login variables
-        self.username_var = tk.StringVar()
-        self.password_var = tk.StringVar()
+        self.username_var = saved_credentials.get("username", "")
 
         # Login components
         ttk.Label(self.login_window, text="Username:").pack(pady=5)
-        ttk.Entry(self.login_window, textvariable=self.username_var).pack(pady=5)
+        self.user_entry = ttk.Entry(self.login_window)
+        self.user_entry.pack(pady=5)
 
         ttk.Label(self.login_window, text="Password:").pack(pady=5)
-        ttk.Entry(self.login_window, textvariable=self.password_var, show="*").pack(pady=5)
+        self.pass_entry = ttk.Entry(self.login_window, show="*")
+        self.pass_entry.pack(pady=5)
 
-        ttk.Button(self.login_window, text="Login", command=self.login).pack(pady=10)
-        ttk.Button(self.login_window, text="Register", command=self.register).pack(pady=5)
-        
+        # Create a frame to contain the login buttons
+        button_frame = ttk.Frame(self.login_window)
+        button_frame.pack(pady=10)
+
+        # Login and Login as User buttons
+        ttk.Button(button_frame, text="Login", command=self.login).pack(side=tk.LEFT, padx=5)
+        ttk.Button(button_frame, text="Login as " + self.username_var, command=self.login_as_user).pack(side=tk.LEFT, padx=5)
+        ttk.Button(self.login_window, text="Register", command=self.register).pack(pady=10)
+
     def login(self):
-        username = self.username_var.get()
-        password = self.password_var.get()
+        username = self.user_entry.get()
+        password = hash(self.pass_entry.get())
 
-        # Validate username and password (you may check against a database)
+        config = configparser.ConfigParser()
+        config["Credentials"] = {"username": username, "password": password}
+        with open("credentials.ini", "w") as configfile:
+            config.write(configfile)
+
         if self.db.login(username, password,):
             self.login_window.destroy()  # Close login window
             self.initialise_app()
         else:
             messagebox.showerror("Login Failed", "Invalid username or password")
 
+    def logout(self):
+        # Load saved credentials
+        saved_credentials = self.load_saved_credentials()
+
+        self.root.destroy()
+
+        root = tk.Tk()
+        self.__init__(root)
+        root.mainloop()
+
+
+    def login_as_user(self):
+        saved_credentials = self.load_saved_credentials()
+
+        username = saved_credentials.get("username", "")
+        password = saved_credentials.get("password", "")
+
+        if self.db.login(username, password,):
+            self.login_window.destroy()  # Close login window
+            self.initialise_app()
+        else:
+            messagebox.showerror("Login Failed", "Invalid username or password")
+
+    def load_saved_credentials(self):
+        import os
+
+        # Load saved credentials from a configuration file
+        config = configparser.ConfigParser()
+        
+        if os.path.exists("credentials.ini"):
+            try:
+                config.read("credentials.ini")
+                credentials = dict(config["Credentials"])
+                
+                return credentials
+            except configparser.Error as e:
+                print("Error loading credentials:", e)
+                return {}
+        else:
+            print("Credentials file does not exist. Creating a new one.")
+            # Create a new credentials file
+            with open("credentials.ini", "w") as configfile:
+                config.add_section("Credentials")
+                config.set("Credentials", "username", "")
+                config.set("Credentials", "password", "")
+                config.write(configfile)
+
+            return {}
+
     def register(self):
-        username = self.username_var.get()
-        password = self.password_var.get()
+        username = self.user_entry.get()
+        password = hash(self.pass_entry.get())
 
         if username:
             if self.db.register_user(username, password) == False:
@@ -62,7 +127,7 @@ class BudgetTrackerApp:
 
         self.init_income()
 
-        
+        self.show_transactions()
 
     def init_categories(self):
          # Categories Page
@@ -74,8 +139,6 @@ class BudgetTrackerApp:
         self.category_combobox.grid(row=0, column=0, padx=10, pady=10)
         self.category_combobox["values"] = self.db.get_category_names()
         self.category_combobox.bind("<<ComboboxSelected>>", self.on_combobox_selected)
-
-        ttk.Button(categories_page, text="See All Expenses", command=self.see_all_expenses).grid(row=0, column=1, padx=10, pady=10)
 
         ttk.Label(categories_page, text="Category Info").grid(row=1, column=0, pady=5, columnspan=2)
 
@@ -108,35 +171,54 @@ class BudgetTrackerApp:
         # Add Income
         ttk.Label(categories_page, text="Add Expense").grid(row=6, column=0, pady=5, columnspan=2)
 
-        ttk.Label(categories_page, text="Amount:").grid(row=7, column=0, pady=5)
-        self.income_entry = ttk.Entry(categories_page, width=15)
-        self.income_entry.grid(row=7, column=1, padx=10, pady=5)
+        ttk.Label(categories_page, text="Description (optional) :").grid(row=7, column=0, pady=5)
+        self.category_name_entry = ttk.Entry(categories_page, width=15)
+        self.category_name_entry.grid(row=7, column=1, padx=10, pady=5)
 
-        ttk.Button(categories_page, text="Add Expense", command=self.add_expense).grid(row=7, column=2, padx=10, pady=5)
+        ttk.Label(categories_page, text="Amount:").grid(row=7, column=2, pady=5)
+        self.category_expense_entry = ttk.Entry(categories_page, width=15)
+        self.category_expense_entry.grid(row=7, column=3, padx=10, pady=5)
+
+        ttk.Button(categories_page, text="Add Expense", command=self.add_expense).grid(row=7, column=4, padx=10, pady=5)
 
         # Expenses Table (Simplified Representation)
         ttk.Label(categories_page, text="Expenses Table").grid(row=8, column=0, pady=10, columnspan=6)
 
-        self.expenses_table = ttk.Treeview(categories_page, columns=("Amount", "Type"))
-        self.expenses_table.heading("#0", text="Date")
+        self.expenses_table = ttk.Treeview(categories_page, columns=("Date", "Amount", "Description"), show="headings")
+        self.expenses_table.heading("Date", text="Date")
         self.expenses_table.heading("Amount", text="Amount")
-        self.expenses_table.heading("Type", text="Type")
+        self.expenses_table.heading("Description", text="Description")
         self.expenses_table.grid(row=9, column=0, columnspan=6, padx=10, pady=5)
 
     def init_dashboard(self):
         # Dashboard Page
         dashboard_page = ttk.Frame(self.notebook)
         self.notebook.add(dashboard_page, text="Dashboard")
+        
+        # Logout button
+        logout_button = ttk.Button(dashboard_page, text="Logout", command=self.logout)
+        logout_button.pack(side=tk.TOP, anchor=tk.NE, padx=10, pady=5)
 
         ttk.Label(dashboard_page, text="Pie Chart of Categories").pack(pady=10)
 
         # Dashboard Info
         ttk.Label(dashboard_page, text="Total Money Available:").pack(pady=5)
+        self.funds_text = ttk.Label(dashboard_page, text="asd")
+        self.funds_text.pack(pady=5)
         ttk.Label(dashboard_page, text="Total Money Assigned:").pack(pady=5)
+
+        self.show_dashboard_info()
 
         # TODO: Add Pie Chart widget (you may use Matplotlib for this)
 
         # TODO: Display the total money available and total money assigned
+
+        self.transaction_table = ttk.Treeview(dashboard_page, columns=("Date", "Categories", "Amount", "Description"), show="headings")
+        self.transaction_table.heading("Date", text="Date")
+        self.transaction_table.heading("Categories", text="Categories")
+        self.transaction_table.heading("Amount", text="Amount")
+        self.transaction_table.heading("Description", text="Description")
+        self.transaction_table.pack(pady=10)
 
     def init_income(self):
          # Categories Page
@@ -144,12 +226,10 @@ class BudgetTrackerApp:
         self.notebook.add(income_page, text="Income")
 
          # Category Info
-        self.income_combobox = ttk.Combobox(income_page, values=self.db.get_category_names(), state="readonly", width=20)
+        self.income_combobox = ttk.Combobox(income_page, values=self.db.get_income_names(), state="readonly", width=20)
         self.income_combobox.grid(row=0, column=0, padx=10, pady=10)
         self.income_combobox["values"] = self.db.get_income_names()
-        self.income_combobox.bind("<<ComboboxSelected>>", self.on_combobox_selected)
-
-        ttk.Button(income_page, text="See All Expenses", command=self.see_all_expenses).grid(row=0, column=1, padx=10, pady=10)
+        self.income_combobox.bind("<<ComboboxSelected>>", self.on_combobox_income_selected)
 
         ttk.Label(income_page, text="Category Info").grid(row=1, column=0, pady=5, columnspan=2)
 
@@ -173,16 +253,20 @@ class BudgetTrackerApp:
         self.new_income_name_entry = ttk.Entry(income_page, width=15)
         self.new_income_name_entry.grid(row=5, column=1, padx=10, pady=5)
 
-        ttk.Button(income_page, text="Add Source", command=self.add_category).grid(row=5, column=2, padx=10, pady=5)
+        ttk.Button(income_page, text="Add Source", command=self.add_source).grid(row=5, column=2, padx=10, pady=5)
 
         # Add Income
         ttk.Label(income_page, text="Add Income").grid(row=6, column=0, pady=5, columnspan=2)
 
-        ttk.Label(income_page, text="Amount:").grid(row=7, column=0, pady=5)
-        self.income_entry = ttk.Entry(income_page, width=15)
-        self.income_entry.grid(row=7, column=1, padx=10, pady=5)
+        ttk.Label(income_page, text="Description (optional) :").grid(row=7, column=0, pady=5)
+        self.income_name_entry = ttk.Entry(income_page, width=15)
+        self.income_name_entry.grid(row=7, column=1, padx=10, pady=5)
 
-        ttk.Button(income_page, text="Add Income", command=self.add_expense).grid(row=7, column=2, padx=10, pady=5)
+        ttk.Label(income_page, text="Amount:").grid(row=7, column=2, pady=5)
+        self.income_amount_entry = ttk.Entry(income_page, width=15)
+        self.income_amount_entry.grid(row=7, column=3, padx=10, pady=5)
+
+        ttk.Button(income_page, text="Add Income", command=self.add_income).grid(row=7, column=4, padx=10, pady=5)
 
     def see_all_expenses(self):
         # TODO: Implement displaying all expenses in the expenses_table
@@ -206,25 +290,62 @@ class BudgetTrackerApp:
             messagebox.showwarning("Input Error", "Please enter category name and budget limit.")
 
     def add_income(self):
-        # TODO: Implement adding income
-        pass
+        selected_category_index = self.income_combobox.current()
+        income_name = self.income_name_entry.get()
+        income_amount = self.income_amount_entry.get()
+
+        income_id = self.db.get_income_type_id(self.income_combobox.get())
+
+        if selected_category_index != -1:
+            if income_amount:
+                self.db.add_income(income_id, income_amount, income_name)
+
+                messagebox.showinfo("Income Added", f"Income of {income_amount} added to {self.income_combobox.get()}")
+
+                self.income_name_entry.delete(0, tk.END)
+                self.income_amount_entry.delete(0, tk.END)
+                
+                self.show_dashboard_info()
+                self.show_transactions()
+            else:
+                messagebox.showwarning("Input Error", "Please enter income amount")
+        else:
+            messagebox.showwarning("Selection Error", "Please select a category")
+
+    def add_source(self): 
+        new_source_name = self.new_income_name_entry.get()
+
+        if new_source_name:
+            if self.db.insert_source(new_source_name) == False:
+                messagebox.showerror("Source Error", "This income type name already exists please choose another")
+            
+            self.income_combobox["values"] = self.db.get_income_names()
+
+            self.new_income_name_entry.delete(0, tk.END)
+        else:
+            messagebox.showwarning("Input Error", "Please enter an income type name.")
 
     def add_expense(self):
         selected_category_index = self.category_combobox.current()
         selected_category = self.db.get_category(self.category_combobox.get())
+
         if selected_category_index != -1:
-            expense_amount = self.expense_entry.get()
+            expense_amount = self.category_expense_entry.get()
+            expense_description = self.category_name_entry.get()
 
             if expense_amount:
-                selected_category.add_expense(float(expense_amount))
-                messagebox.showinfo("Expense Added", f"Expense of {expense_amount} added to {selected_category.name}")
-                self.expense_entry.delete(0, tk.END)
+                self.db.add_expense(selected_category[0], expense_amount, expense_description)
+                messagebox.showinfo("Expense Added", f"Expense of {expense_amount} added to {selected_category[1]}")
+                self.category_expense_entry.delete(0, tk.END)
                 self.show_category_info(selected_category_index)
                 # TODO: Update expenses_table
             else:
                 messagebox.showwarning("Input Error", "Please enter expense amount.")
         else:
             messagebox.showwarning("Selection Error", "Please select a category.")
+
+        self.show_dashboard_info()
+        self.show_transactions()
 
     def show_category_info(self, selected_category_index):
         if selected_category_index != -1:
@@ -236,6 +357,54 @@ class BudgetTrackerApp:
             self.category_info_labels[2]["text"] = f"{selected_category[2] - selected_category[3]}"
         else:
             messagebox.showwarning("Selection Error", "Please select a category.")
+
+    def show_income_info(self, selected_category_index):
+        print(0)
+
+    def show_dashboard_info(self):
+        self.funds_text["text"] = self.db.get_funds()
     
     def on_combobox_selected(self, event):
         self.show_category_info(self.category_combobox.current())
+
+        self.show_expenses(self.db.get_category(self.category_combobox.get())[0])
+
+    def on_combobox_income_selected(self, event):
+        self.show_income_info(self.income_combobox.current())
+
+    def show_expenses(self, category_id):
+        # Clear existing entries in the expenses_table
+        for item in self.expenses_table.get_children():
+            self.expenses_table.delete(item)
+
+        # Fetch expenses for the selected category from the database
+        expenses = self.db.get_expenses(category_id)
+
+        # Insert expenses into the expenses_table
+        for expense in expenses:
+            self.expenses_table.insert("", 0, values=(expense[0], expense[1], expense[2],))
+
+    def show_transactions(self):
+        # Clear existing entries in the expenses_table
+        for item in self.transaction_table.get_children():
+            self.transaction_table.delete(item)
+
+        # Fetch expenses for the selected category from the database
+        expenses = self.db.get_transactions()
+
+        # Insert expenses into the expenses_table
+        for expense in expenses:
+            self.transaction_table.insert("", 0, values=(expense[0], expense[3], expense[1], expense[2]))
+
+    def hash(input_string):
+        hash_value = 5381
+
+        for char in input_string:
+            hash_value = (hash_value * 33) ^ ord(char)
+
+        # Apply additional mixing steps (XOR and bit shifting)
+        hash_value = (hash_value ^ (hash_value >> 15)) * 2654435761 & 0xFFFFFFFF
+        hash_value = (hash_value ^ (hash_value >> 13)) * 2654435761 & 0xFFFFFFFF
+        hash_value = hash_value ^ (hash_value >> 16)
+
+        return hash_value
